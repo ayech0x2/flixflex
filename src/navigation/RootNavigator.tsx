fd55e0748ea74c36f0e8f065c3d03dd77a358b79
Hamza Ayech <4yech.hamza@gmail.com>
2025-09-10 06:44:06 +0100

    Condtionnaly rendering HomeNavigator and AuthNavigator

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,8 +1,11 @@
 import AuthNavigator from "@/features/auth/navigators/AuthNavigator";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { RootNavigatorParamList } from "./types";
-import { useTheme } from "@shopify/restyle";
+import HomeNavigator from "@/features/home/navigators/HomeNavigator";
 import { Theme } from "@/lib/restyle";
+import { userAtom } from "@/shared/atoms";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useTheme } from "@shopify/restyle";
+import { useAtomValue } from "jotai";
+import { RootNavigatorParamList } from "./types";
 
 const Stack = createNativeStackNavigator<RootNavigatorParamList>();
 
@@ -10,6 +13,17 @@ function RootNavigator() {
   const {
     colors: { appBg },
   } = useTheme<Theme>();
+
+  const user = useAtomValue(userAtom);
+
+  const renderNavigator = () => {
+    if (user) {
+      return <Stack.Screen name="HomeNavigator" component={HomeNavigator} />;
+    } else {
+      return <Stack.Screen name="AuthNavigator" component={AuthNavigator} />;
+    }
+  };
+
   return (
     <Stack.Navigator
       screenOptions={{
@@ -17,7 +31,7 @@ function RootNavigator() {
         contentStyle: { backgroundColor: appBg },
       }}
     >
-      <Stack.Screen name="AuthNavigator" component={AuthNavigator} />
+      {renderNavigator()}
     </Stack.Navigator>
   );
 }
