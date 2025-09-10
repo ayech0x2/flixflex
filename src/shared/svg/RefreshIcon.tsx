cdb1a8d9088fed3333680c0b0c81a6f4badf08a1
Hamza Ayech <4yech.hamza@gmail.com>
2025-09-10 02:19:10 +0100

    Adding RefreshIcon

---
+++ src/shared/svg/RefreshIcon.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+import { IconProps } from "../types";
+import useIcon from "../hooks/useIcon";
+
+function RefreshIcon(props: IconProps) {
+  const { size, color, strokeWidth } = useIcon(props);
+  return (
+    <Svg width={size} height={size} viewBox="0 0 24 24" fill="none" {...props}>
+      <Path
+        d="M22 12c0 5.52-4.48 10-10 10s-8.89-5.56-8.89-5.56m0 0h4.52m-4.52 0v5M2 12C2 6.48 6.44 2 12 2c6.67 0 10 5.56 10 5.56m0 0v-5m0 5h-4.44"
+        stroke={color}
+        strokeWidth={strokeWidth}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+}
+
+export default RefreshIcon;
