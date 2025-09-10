b1742551182a127e070518698970258019957fd4
Hamza Ayech <4yech.hamza@gmail.com>
2025-09-10 06:45:02 +0100

    Condtionnaly rendering Sync and Auth screens

--- src/features/auth/navigators/AuthNavigator.tsx
+++ src/features/auth/navigators/AuthNavigator.tsx
@@ -2,7 +2,10 @@ import { Theme } from "@/lib/restyle";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { useTheme } from "@shopify/restyle";
 import AuthScreen from "../screens/AuthScreen";
+import SyncScreen from "../screens/SyncScreen";
 import { AuthNavigatorParamList } from "../types";
+import { userAtom } from "@/shared/atoms";
+import { useAtomValue } from "jotai";
 
 const Stack = createNativeStackNavigator<AuthNavigatorParamList>();
 
@@ -10,15 +13,23 @@ function AuthNavigator() {
   const {
     colors: { appBg },
   } = useTheme<Theme>();
+  const user = useAtomValue(userAtom);
+
+  const renderScreen = () => {
+    if (user === undefined) {
+      return <Stack.Screen name="Sync" component={SyncScreen} />;
+    } else {
+      return <Stack.Screen name="Auth" component={AuthScreen} />;
+    }
+  };
   return (
     <Stack.Navigator
-      initialRouteName="Auth"
       screenOptions={{
         headerShown: false,
         contentStyle: { backgroundColor: appBg },
       }}
     >
-      <Stack.Screen name="Auth" component={AuthScreen} />
+      {renderScreen()}
     </Stack.Navigator>
   );
 }
