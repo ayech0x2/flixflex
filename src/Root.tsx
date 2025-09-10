acf2e9d52db61ca748a0f49f48c1fe99ef47c528
Hamza Ayech <4yech.hamza@gmail.com>
2025-09-10 06:40:45 +0100

    Adding more Root providers

--- src/Root.tsx
+++ src/Root.tsx
@@ -2,11 +2,14 @@ import { NavigationContainer } from "@react-navigation/native";
 import { ThemeProvider } from "@shopify/restyle";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StatusBar } from "expo-status-bar";
-import { useAtomValue } from "jotai";
+import { Provider as JotaiProvider, useAtomValue } from "jotai";
 import * as React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { store } from "./lib/jotai";
 import { darkTheme, lightTheme } from "./lib/restyle";
 import RootNavigator from "./navigation/RootNavigator";
 import { themeAtom } from "./shared/atoms";
+import Alert from "./shared/components/Alert";
 
 function Root() {
   const theme = useAtomValue(themeAtom);
@@ -15,11 +18,16 @@ function Root() {
   return (
     <ThemeProvider theme={theme === "DARK" ? darkTheme : lightTheme}>
       <StatusBar style={theme === "DARK" ? "light" : "dark"} />
-      <NavigationContainer>
-        <QueryClientProvider client={client}>
-          <RootNavigator />
-        </QueryClientProvider>
-      </NavigationContainer>
+      <JotaiProvider store={store}>
+        <NavigationContainer>
+          <QueryClientProvider client={client}>
+            <SafeAreaProvider>
+              <Alert />
+              <RootNavigator />
+            </SafeAreaProvider>
+          </QueryClientProvider>
+        </NavigationContainer>
+      </JotaiProvider>
     </ThemeProvider>
   );
 }
