ae7b727f2818abff46eb19a10c6a2e00d6f390d8
Hamza Ayech <4yech.hamza@gmail.com>
2025-09-10 22:43:28 +0100

    Enhancing profile screen and navigator

--- src/features/profile/navigators/ProfileNavigator.tsx
+++ src/features/profile/navigators/ProfileNavigator.tsx
@@ -1,11 +1,22 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { ProfileNavigatorParamList } from "../types";
 import ProfileScreen from "../screens/ProfileScreen";
+import { Theme } from "@/lib/restyle";
+import { useTheme } from "@shopify/restyle";
 
 function ProfileNavigator() {
   const Stack = createNativeStackNavigator<ProfileNavigatorParamList>();
+  const {
+    colors: { appBg },
+  } = useTheme<Theme>();
+
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: appBg },
+      }}
+    >
       <Stack.Screen name="Profile" component={ProfileScreen} />
     </Stack.Navigator>
   );
--- src/features/profile/screens/ProfileScreen.tsx
+++ src/features/profile/screens/ProfileScreen.tsx
@@ -1,10 +1,67 @@
-import { Box, Text } from "@/lib/restyle";
+import { useAuth } from "@/features/auth/services/useAuth";
+import { moderateScale } from "@/lib/normalize";
+import { Box, Text, TouchableOpacity } from "@/lib/restyle";
+import useAlert from "@/shared/hooks/useAlert";
+import { useSignout } from "@/shared/services/useSignout";
+import UserIcon from "@/shared/svg/UserIcon";
+import { Alert } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 function ProfileScreen() {
+  const { data: user } = useAuth();
+
+  const { mutate: signout } = useSignout();
+
+  const { show } = useAlert();
+
+  const handleSignout = () => {
+    Alert.alert("Logout", "Are you sure you want to logout?", [
+      { text: "Cancel", style: "cancel" },
+      {
+        text: "Logout",
+        onPress: () =>
+          signout(undefined, {
+            onSuccess: () => {
+              show({
+                type: "SUCCESS",
+                title: "Logout",
+                message: "You have been logged out",
+              });
+            },
+          }),
+      },
+    ]);
+  };
+
   return (
-    <Box>
-      <Text>Profile</Text>
-    </Box>
+    <SafeAreaView>
+      <Box paddingTop="xxl" justifyContent="center" alignItems="center" gap="m">
+        <Box
+          height={moderateScale(100)}
+          width={moderateScale(100)}
+          justifyContent="center"
+          alignItems="center"
+          backgroundColor="inputBg"
+          style={{
+            borderRadius: moderateScale(100) / 2,
+          }}
+        >
+          <UserIcon size={moderateScale(40)} color="bodyText" />
+        </Box>
+        <Text color="secondaryBodyText">{user?.email}</Text>
+        <TouchableOpacity
+          paddingHorizontal="m"
+          backgroundColor="danger"
+          paddingVertical="s"
+          borderRadius="button"
+          onPress={handleSignout}
+        >
+          <Text color="buttonText" variant="button">
+            Logout
+          </Text>
+        </TouchableOpacity>
+      </Box>
+    </SafeAreaView>
   );
 }
 
