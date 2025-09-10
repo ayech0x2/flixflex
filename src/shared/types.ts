362835ee14585a86980fda8e5c0f3056c7247248
Hamza Ayech <4yech.hamza@gmail.com>
2025-09-10 02:23:39 +0100

    Utils types

---
+++ src/shared/types.ts
@@ -0,0 +1,18 @@
+import { Theme } from "@/lib/restyle";
+import { StyleProp, ViewStyle } from "react-native";
+
+export type IconSize =
+  | "tiny"
+  | "small"
+  | "medium"
+  | "large"
+  | "xLarge"
+  | number;
+
+export interface IconProps {
+  color?: keyof Theme["colors"] | undefined;
+  fillColor?: keyof Theme["colors"] | undefined;
+  size?: IconSize | undefined;
+  strokeWidth?: number | undefined;
+  style?: StyleProp<ViewStyle> | undefined;
+}
