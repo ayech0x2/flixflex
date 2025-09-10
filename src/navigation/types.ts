28ced41d1dd38124fd8b1f70d981995d9ca9e08f
Hamza Ayech <4yech.hamza@gmail.com>
2025-09-10 06:43:04 +0100

    Pushing HomeNavigator inside the RootNavigator type

--- src/navigation/types.ts
+++ src/navigation/types.ts
@@ -3,6 +3,7 @@ import { NavigatorScreenParams } from "@react-navigation/native";
 
 type RootNavigatorParamList = {
   AuthNavigator: NavigatorScreenParams<AuthNavigatorParamList>;
+  HomeNavigator: NavigatorScreenParams<AuthNavigatorParamList>;
 };
 
 export { RootNavigatorParamList };
