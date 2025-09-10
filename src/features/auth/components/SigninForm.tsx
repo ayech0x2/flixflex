00701af4dbef1146d2dedbf91b0ebe60e857a382
Hamza Ayech <4yech.hamza@gmail.com>
2025-09-10 06:45:51 +0100

    Handling validation and refactoring messages

--- src/features/auth/components/SigninForm.tsx
+++ src/features/auth/components/SigninForm.tsx
@@ -1,24 +1,67 @@
 import { Box, Text, TouchableOpacity } from "@/lib/restyle";
 import Button from "@/shared/components/Button";
 import TextInput from "@/shared/components/TextInput";
+import useAlert from "@/shared/hooks/useAlert";
+import useExtractValidationMessages from "@/shared/hooks/useExtractValidationMessages";
+import useFormError from "@/shared/hooks/useFormError";
 import EyeIcon from "@/shared/svg/EyeIcon";
 import EyeSlashIcon from "@/shared/svg/EyeSlashIcon";
 import LockIcon from "@/shared/svg/LockIcon";
-import UserIcon from "@/shared/svg/UserIcon";
+import MailIcon from "@/shared/svg/MailIcon";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as React from "react";
-import { useSignin } from "../services/useSignin";
 import { Controller, useForm } from "react-hook-form";
+import { signinSchema } from "../schemas";
+import { useSignin } from "../services/useSignin";
 import { SigninInput } from "../types";
 
 function SigninForm({ navigateToSignup }: { navigateToSignup: () => void }) {
   const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
 
-  const { handleSubmit, control } = useForm<SigninInput>();
+  const { show } = useAlert();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<SigninInput>({
+    resolver: yupResolver(signinSchema),
+  });
+
+  const validation = useExtractValidationMessages<SigninInput>(errors);
 
   const { mutate: signin, isPending, error } = useSignin();
-  console.log(error);
+
+  const { validationError } = useFormError(validation, error);
+
+  React.useEffect(() => {
+    if (validationError) {
+      show({
+        type: "DANGER",
+        title: "Validation error",
+        message: "Please check your information",
+      });
+      return;
+    }
+  }, [show, validationError]);
+
   const onSubmit = (d: SigninInput) => {
-    signin(d);
+    signin(d, {
+      onSuccess: () => {
+        show({
+          type: "SUCCESS",
+          title: "Congratulations",
+          message: "Account signed in successfully",
+        });
+      },
+      onError: () => {
+        show({
+          type: "DANGER",
+          title: "Submission error",
+          message: "Invalid email or password",
+        });
+      },
+    });
   };
 
   return (
@@ -29,11 +72,12 @@ function SigninForm({ navigateToSignup }: { navigateToSignup: () => void }) {
         control={control}
         render={({ field: { onChange: onChangeText } }) => (
           <TextInput
-            left={<UserIcon color="inputIcon" />}
-            label="Username"
+            left={<MailIcon color="inputIcon" />}
+            label="Address email"
+            error={validation.email}
             nativeProps={{
               onChangeText,
-              placeholder: "Ex: ayech0x2",
+              placeholder: "Ex: 4yech.hamza@gmail.com",
             }}
           />
         )}
@@ -56,6 +100,7 @@ function SigninForm({ navigateToSignup }: { navigateToSignup: () => void }) {
               </TouchableOpacity>
             }
             label="Password"
+            error={validation.password}
             nativeProps={{
               onChangeText,
               placeholder: "********",
--- src/features/auth/components/SignupForm.tsx
+++ src/features/auth/components/SignupForm.tsx
@@ -1,62 +1,138 @@
 import { Box, Text, TouchableOpacity } from "@/lib/restyle";
 import Button from "@/shared/components/Button";
 import TextInput from "@/shared/components/TextInput";
+import useAlert from "@/shared/hooks/useAlert";
+import useExtractValidationMessages from "@/shared/hooks/useExtractValidationMessages";
+import useFormError from "@/shared/hooks/useFormError";
 import EyeIcon from "@/shared/svg/EyeIcon";
 import EyeSlashIcon from "@/shared/svg/EyeSlashIcon";
 import LockIcon from "@/shared/svg/LockIcon";
 import UserIcon from "@/shared/svg/UserIcon";
+import { yupResolver } from "@hookform/resolvers/yup";
 import * as React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { signupSchema } from "../schemas";
+import { useSignup } from "../services/useSignup";
+import { SignupInput } from "../types";
+import { useQueryClient } from "@tanstack/react-query";
+import { USE_AUTH_QUERY_KEY } from "@/shared/services/keys";
 
 function SignupForm({ navigateToSignin }: { navigateToSignin: () => void }) {
   const [isPasswordVisible, setIsPasswordVisible] = React.useState(false);
 
+  const client = useQueryClient();
+
+  const { show } = useAlert();
+
+  const {
+    handleSubmit,
+    control,
+    formState: { errors },
+  } = useForm<SignupInput>({
+    resolver: yupResolver(signupSchema),
+  });
+
+  const validation = useExtractValidationMessages<SignupInput>(errors);
+
+  const { mutate: signup, isPending, error } = useSignup();
+
+  const { validationError } = useFormError(validation, error);
+
+  React.useEffect(() => {
+    if (validationError) {
+      show({
+        type: "DANGER",
+        title: "Validation error",
+        message: "Please check your information",
+      });
+      return;
+    }
+  }, [show, validationError]);
+
+  const onSubmit = (d: SignupInput) => {
+    signup(d, {
+      onSuccess: () => {
+        show({
+          type: "SUCCESS",
+          title: "Congratulations",
+          message: "Account created successfully",
+        });
+        client.invalidateQueries({ queryKey: [USE_AUTH_QUERY_KEY] });
+      },
+      onError: () => {
+        show({
+          type: "DANGER",
+          title: "Submission error",
+          message: "Something went wrong",
+        });
+      },
+    });
+  };
+
   return (
     <Box paddingHorizontal="screenPadding" gap="m">
-      <Text variant="title">Create your{"\n"}Account</Text>
-      <TextInput
-        left={<UserIcon color="inputIcon" />}
-        label="Email address"
-        nativeProps={{
-          placeholder: "Ex: 4yech.hamza@gmail.com",
-        }}
+      <Text variant="title">Create your {"\n"}Account</Text>
+      <Controller
+        name="email"
+        control={control}
+        render={({ field: { onChange: onChangeText } }) => (
+          <TextInput
+            left={<UserIcon color="inputIcon" />}
+            label="Address email"
+            error={validation.email}
+            nativeProps={{
+              onChangeText,
+              placeholder: "Ex: 4yech.hamza@gmail.com",
+            }}
+          />
+        )}
       />
-      <TextInput
-        left={<UserIcon color="inputIcon" />}
-        label="Username"
-        nativeProps={{
-          placeholder: "Ex: ayech0x2",
-        }}
+      <Controller
+        name="password"
+        control={control}
+        render={({ field: { onChange: onChangeText } }) => (
+          <TextInput
+            left={<LockIcon color="inputIcon" />}
+            right={
+              <TouchableOpacity
+                onPress={() => setIsPasswordVisible(!isPasswordVisible)}
+              >
+                {isPasswordVisible ? (
+                  <EyeSlashIcon color="inputIcon" />
+                ) : (
+                  <EyeIcon color="inputIcon" />
+                )}
+              </TouchableOpacity>
+            }
+            label="Password"
+            error={validation["password"]}
+            nativeProps={{
+              onChangeText,
+              placeholder: "********",
+              secureTextEntry: !isPasswordVisible,
+            }}
+          />
+        )}
       />
-      <TextInput
-        left={<LockIcon color="inputIcon" />}
-        right={
-          <TouchableOpacity
-            onPress={() => setIsPasswordVisible(!isPasswordVisible)}
-          >
-            {isPasswordVisible ? (
-              <EyeSlashIcon color="inputIcon" />
-            ) : (
-              <EyeIcon color="inputIcon" />
-            )}
-          </TouchableOpacity>
-        }
-        label="Password"
-        nativeProps={{
-          placeholder: "********",
-          secureTextEntry: !isPasswordVisible,
-        }}
+      <Controller
+        name="confirmPassword"
+        control={control}
+        render={({ field: { onChange: onChangeText } }) => (
+          <TextInput
+            left={<LockIcon color="inputIcon" />}
+            label="Confirm Password"
+            error={validation.confirmPassword}
+            nativeProps={{
+              onChangeText,
+              placeholder: "********",
+              secureTextEntry: !isPasswordVisible,
+            }}
+          />
+        )}
       />
-      <TextInput
-        left={<LockIcon color="inputIcon" />}
-        label="Password confirmation"
-        nativeProps={{
-          placeholder: "********",
-          secureTextEntry: !isPasswordVisible,
-        }}
-      />
-      <Button>
-        <Text variant="button" textTransform="uppercase">
-          Create my account
+      <Button onPress={handleSubmit(onSubmit)} loading={isPending}>
+        <Text textTransform="uppercase" variant="button">
+          Connect
         </Text>
       </Button>
       <Text color="secondaryBodyText" textAlign="center">
