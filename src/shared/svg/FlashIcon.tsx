596637bd7646cb700872be93b1e766003f7f9461
Hamza Ayech <4yech.hamza@gmail.com>
2025-09-10 07:42:06 +0100

    Adding flashIcon

---
+++ src/shared/svg/FlashIcon.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+import Svg, { Path } from "react-native-svg";
+import { IconProps } from "../types";
+import useIcon from "../hooks/useIcon";
+
+function FlashIcon(props: IconProps) {
+  const { size, color, strokeWidth } = useIcon(props);
+  return (
+    <Svg width={size} height={size} viewBox="0 0 24 24" fill="none">
+      <Path
+        d="M6.09 13.28h3.09v7.2c0 1.68.91 2.02 2.02.76l7.57-8.6c.93-1.05.54-1.92-.87-1.92h-3.09v-7.2c0-1.68-.91-2.02-2.02-.76l-7.57 8.6c-.92 1.06-.53 1.92.87 1.92z"
+        stroke={color}
+        strokeWidth={strokeWidth}
+        strokeMiterlimit={10}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </Svg>
+  );
+}
+
+export default FlashIcon;
