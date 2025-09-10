074a04abd30a2f73ac012d6d435374b608a694c0
Hamza Ayech <4yech.hamza@gmail.com>
2025-09-10 10:31:25 +0100

    Adapt TMDB returned data to the MediaItem

--- src/shared/components/MediaItem.tsx
+++ src/shared/components/MediaItem.tsx
@@ -1,32 +1,43 @@
 import { moderateScale } from "@/lib/normalize";
 import { Box, Image, Text } from "@/lib/restyle";
 import StarIcon from "@/shared/svg/StarIcon";
-import { MediaItemProps } from "@/shared/types";
+import { TmdbItem } from "../types";
 
-const WIDTH = moderateScale(120);
+const PORTRAIT_WIDTH = moderateScale(120);
+const PORTRAIT_HEIGHT = PORTRAIT_WIDTH * 1.5;
 
-function MediaItem({ title, year, rating, image }: MediaItemProps) {
+const LANDSCAPE_WIDTH = moderateScale(250);
+const LANDSCAPE_HEIGHT = LANDSCAPE_WIDTH / 1.77;
+
+function MediaItem(props: TmdbItem & { mode: "PORTRAIT" | "LANDSCAPE" }) {
+  const title = "title" in props ? props.title : props.name;
+  const date =
+    "release_date" in props ? props.release_date : props.first_air_date;
+
   return (
     <Box gap="xs">
       <Image
-        width={WIDTH}
-        height={WIDTH * 1.5}
+        width={props.mode === "PORTRAIT" ? PORTRAIT_WIDTH : LANDSCAPE_WIDTH}
+        height={props.mode === "PORTRAIT" ? PORTRAIT_HEIGHT : LANDSCAPE_HEIGHT}
         source={{
-          uri: image,
+          uri: `https://image.tmdb.org/t/p/w185${props.poster_path}`,
         }}
       />
-      <Text numberOfLines={1} maxWidth={WIDTH}>
+      <Text
+        numberOfLines={1}
+        maxWidth={props.mode === "PORTRAIT" ? PORTRAIT_WIDTH : LANDSCAPE_WIDTH}
+      >
         {title}
       </Text>
       <Box flexDirection="row" justifyContent="space-between">
-        <Text variant="bodySecondary">{year}</Text>
+        <Text variant="bodySecondary">{new Date(date).getFullYear()}</Text>
         <Box flexDirection="row" alignItems="center" gap="xs">
           <StarIcon
             color="accent"
             fillColor="accent"
             size={moderateScale(12)}
           />
-          <Text variant="bodySecondary">{rating}</Text>
+          <Text variant="bodySecondary">{props.vote_average.toFixed(1)}</Text>
         </Box>
       </Box>
     </Box>
