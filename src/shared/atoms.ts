7e283627c658bf0cab3b93da092df757d19e9258
Hamza Ayech <4yech.hamza@gmail.com>
2025-09-10 06:41:49 +0100

    Alert and User atoms implementation

--- src/shared/atoms.ts
+++ src/shared/atoms.ts
@@ -1,5 +1,11 @@
 import { atom } from "jotai";
+import { AlertPayload } from "./types";
+import { User } from "firebase/auth";
 
 const themeAtom = atom<"LIGHT" | "DARK">("DARK");
 
-export { themeAtom };
+const alertAtom = atom<AlertPayload | null>(null);
+
+const userAtom = atom<User | null | undefined>();
+
+export { themeAtom, alertAtom, userAtom };
